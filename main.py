2ce8e974f01f2790c3d57f59de56b4065d844302
Ail0711 <ailton.des@uanl.edu.mx>
2024-11-10 23:54:24 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ import argparse
 import hashlib
 import os
 import platform
+import subprocess
 from datetime import datetime
 
 # Crear un objeto ArgumentParser
 parser = argparse.ArgumentParser(description="Aqui para entrar con el argparse")
 
 # Agregar argumentos de entrada
-parser.add_argument('argumento1', type=int, help="Primer argumento")
-parser.add_argument('argumento2', type=int, help="Segundo argumento")
-parser.add_argument('--accion', type=str, choices=['non', 'non'], default='def', help="Operación a realizar")
+parser.add_argument('argum1', type=str, help="Tipo de lenguaje -Poweshell, Bash o Python")
+parser.add_argument('--accion', type=str, choices=['menu', 'script'], default='def', help="Entrar a un menú o iniciar un script")
 
 # Parsear los argumentos de entrada
 args = parser.parse_args()
 
-def os_verif():
+def so_verif():
     sistema = platform.system()
     if sistema == "Windows":
         print("Sistema operativo: Windows")
@@ -26,7 +26,24 @@ def os_verif():
     else:
         print(f"Sistema operativo no reconocido: {sistema}")
         return None
-    
+
+def main():
+    so = so_verif
+    if argparse.accion=='menu':
+        if so=="Windows":
+            if argparse.argum1=='powershell':
+                print
+            elif argparse.argum1=='bash':
+                print
+            elif argparse.argum1=='Pyhton':
+                print
+        elif so == "Linux":
+            if argparse.argum1=='bash':
+                print
+            elif argparse.argum1=='Pyhton':
+                print
+    elif argparse.accion=='script':
+
 def generar_hash_archivo(file_path):
     hash_md5 = hashlib.md5()
     with open(file_path, "rb") as f:
@@ -34,6 +51,22 @@ def generar_hash_archivo(file_path):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
 
+#-------------------------------------------------------------------------------- para poweshell asi deberia de funcionar creo 
+ruta_script = "C:\\ruta al script.ps1"
+
+# Comando para ejecutar el script de PowerShell
+try:
+    resultado = subprocess.run(["powershell", "-ExecutionPolicy", "Bypass", "-File", ruta_script], 
+                               capture_output=True, text=True, check=True)
+    # Mostrar la salida en la terminal
+    print("Salida del script:")
+    print(resultado.stdout)
+except subprocess.CalledProcessError as e:
+    print("Error al ejecutar el script:")
+    print(e.stderr)
+#--------------------------------------------------------------------------------
+
+    hash_resultado = generar_hash_archivo(nombre_archivo)
     nombre_archivo = f"reporte.txt" #aqui ponemos el nombre del script 
     print(f"Tarea ejecutada: nombre del sript")
     print(f"Fecha: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
